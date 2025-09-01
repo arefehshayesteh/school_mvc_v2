7f9da123110d91c125a9805530901319696e9c7a
arefeh <arefehshayestehnik2117@gmail.com>
2025-09-01 03:05:08 -1200

    update messages

---
+++ messages/errors.py
@@ -0,0 +1,13 @@
+class ErrorMessages:
+    INVALID_CLASS_NAME = "نام کلاس نمی‌تواند خالی باشد."
+    INVALID_STUDENT_NAME = "نام دانش اموز نمی‌تواند خالی باشد."
+    INVALID_CODE_NAME = "فیلد کدملی نمی‌تواند خالی باشد."
+    INVALID_CAPACITY_NAME = "ظرفیت کلاس نمی تواند خالی باشد"
+    IF_NOT_WORD = "نام فقط باید شامل حروف باشد."
+    INVALID_CODE = "کدملی باید شامل 10 عدد باشد."
+    CODE_NOT_INTIGER = "فیلد کدملی را به صورت رقم وارد کنید"
+    INVALID_CAPACITY = "ظرفیت کلاس باید عددی مثبت باشد."
+    CLASS_NOT_FOUND = "کلاسی با این شناسه یافت نشد."
+    STUDENT_NOT_FOUND = "دانش‌آموزی با این شناسه یافت نشد."
+    DUPLICATE_ENROLLMENT = "دانش‌آموز قبلاً در این کلاس ثبت‌نام شده است."
+    
