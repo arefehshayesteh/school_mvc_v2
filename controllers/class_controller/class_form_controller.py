999e10d1bbb146b39a622307b85591e0b8885dac
Arefeh <arefehshayestehnik2117@gmail.com>
2025-09-16 15:08:19 +0330

    craete class form and list controller

---
+++ controllers/class_controller/class_form_controller.py
@@ -0,0 +1,20 @@
+from messages.errors import ErrorMessages
+from tkinter import messagebox
+
+class ClassFormController:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        self.view.save_button.config(command= self.save_form)
+
+    def save_form(self):
+        try: 
+            data = self.view.get_class_form()
+            self.model.add_class(data["name"], data["capacity"])
+            raise ValueError(ErrorMessages.SUCCESSFUL_SUBMIT)
+        except Exception as e:
+            raise ValueError(ErrorMessages.UNSUCCESSFUL_SUBMIT)
+
+
+    def get_classes(self):
+        return self.model.get_all_classes()
